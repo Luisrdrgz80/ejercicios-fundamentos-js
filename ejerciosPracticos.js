95980e33d77ea5d98f2e68cf47abaa228b7b5c7c
Luis Rodriguez <luisrdrgz.ing@gmail.com>
2019-03-06 10:14:57 +0100

    Mi nuevo archivo para practicar

---
+++ ejerciosPracticos.js
@@ -0,0 +1,11 @@
+function mostrarDeDos(hasta) {
+    for (i = 0; i <= hasta; i += 2) {
+        if (i === 4) {
+            console.log(i);
+
+        }
+        console.log(i);
+    }
+}
+
+mostrarDeDos(10);
